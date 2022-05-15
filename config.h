3702deb91b23cc9cc772fa4c0f6623f84062dfbb
Sik0M0D# <98068234+Sik0M0D3@users.noreply.github.com>
2022-05-14 23:59:02 -0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+/* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
+/* Maximum character lenght of commands' output */
+#define CMDLENGTH 100
+
+static const Block blocks[] = {
+    /*  Icon      Command             Interval      Signal    */
+    {   "",       "sb-volume",        0,            10        },
+    {   "",       "sb-network",       1,            4         },
+    {   "",       "sb-clock",         60,           1         },
+};
+
+/* Sets delimiter between status commands. NULL character ('\0') means no delimiter. */
+static char *delim = "";
+
+/* Have dwmblocks automatically recompile and run when you edit this file in
+ vim with the following line in your vimrc/init.vim: */
+
+/* autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install
+&& { killall -q dwmblocks;setsid dwmblocks & } */
