403d5662acbce54f40601285afb6db3cd04fbc86
Sik0M0D# <98068234+Sik0M0D3@users.noreply.github.com>
2022-05-31 00:36:44 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
 /* Maximum character lenght of commands' output */
-#define CMDLENGTH 100
+#define CMDLENGTH 124
 
 static const Block blocks[] = {
     /*  Icon      Command             Interval      Signal    */
     {   "",       "sb-volume",        0,            10        },
-    {   "",       "sb-network",       1,            4         },
-    {   "",       "sb-clock",         60,           1         },
+    {   "",       "sb-network",       1,            6         },
+    {   "",       "sb-clock",         60,           4         },
 };
 
 /* Sets delimiter between status commands. NULL character ('\0') means no delimiter. */
